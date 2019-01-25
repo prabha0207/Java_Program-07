42331887af2a868e2426d67badcfef075808132a
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2019-01-25 12:31:35 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Scanner;
+class Power
+{
+   public static void main(String arg[])
+   {
+	  long n,p,r=1;
+	  Scanner sc=new Scanner(System.in);
+    System.out.println(" ");
+	   n=sc.nextLong();
+     System.out.println(" ");
+	   p=sc.nextLong();
+     if(n>=0&&p==0)
+      {   
+          r =1;
+      }
+     else if(n==0&&p>=1)
+	    {   
+          r=0;
+	    } 
+     else
+	    { 
+        for(int i=1;i<=p;i++)
+	       { 
+           r=r *n;
+          }    
+  	  }    
+      System.out.println(n+"^"+p+"="+r);
+     }
+  }
